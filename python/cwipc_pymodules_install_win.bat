becf2e332440aae2f31920995b7afdd283943903
Patrice ANGOT <patrice.angot@viaccess-orca.com>
2020-07-22 16:35:29 +0200

    cd command replace with pushd command in case of drive change. python3 command replace with python command

--- python/cwipc_pymodules_install_win.bat
+++ python/cwipc_pymodules_install_win.bat
@@ -1,7 +1,7 @@
 set bindir=%~dp0
-cd %bindir%\..\share\cwipc_util\python
-python3 setup.py install
-cd %bindir%\..\share\cwipc_realsense2\python
-python3 setup.py install
-cd %bindir%\..\share\cwipc_codec\python
-python3 setup.py install
+pushd %bindir%\..\share\cwipc_util\python
+python setup.py install
+pushd %bindir%\..\share\cwipc_realsense2\python
+python setup.py install
+pushd %bindir%\..\share\cwipc_codec\python
+python setup.py install
