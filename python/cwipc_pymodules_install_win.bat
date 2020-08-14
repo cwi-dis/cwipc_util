07ceffce38b8e51141e3cddcc472abdcac26093d
Patrice ANGOT <patrice.angot@viaccess-orca.com>
2020-08-14 16:42:36 +0200

    commands for running the BAT file as admin have been added

--- python/cwipc_pymodules_install_win.bat
+++ python/cwipc_pymodules_install_win.bat
@@ -1,3 +1,7 @@
+@echo off
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
 set bindir=%~dp0
 pushd %bindir%..\share\cwipc_util\python
 python setup.py install
@@ -5,3 +9,5 @@ pushd %bindir%..\share\cwipc_realsense2\python
 python setup.py install
 pushd %bindir%..\share\cwipc_codec\python
 python setup.py install
+
+exit
