ac3b71c239465cfb1675647741a2e94507610115
Jack Jansen <Jack.Jansen@cwi.nl>
2020-10-13 10:55:19 +0200

    Changed handing of coars targets to use --target option. Added a4 target.

--- python/cwipc/scripts/cwipc_calibrate/__main__.py
+++ python/cwipc/scripts/cwipc_calibrate/__main__.py
@@ -5,57 +5,7 @@ from .calibrator import Calibrator
 from .filegrabber import FileGrabber
 from .livegrabber import LiveGrabber
 from .cameraconfig import DEFAULT_FILENAME
-#
-# Version 2 calibration cross: the one made with little balls and the forward spar
-#
-POINTS_V2 = [
-    (0.25, 1, 0, 255, 0, 0),        # Red ball at Right of the cross
-    (0, 1.25, 0, 0, 0, 255),        # Blue ball at top of the cross (because the sky is blue)
-    (0, 1.1, -0.25, 255, 127, 0),   # Orange ball pointing towards the viewer (-Z) because everyone likes orange
-    (-0.25, 1, 0, 255, 255, 0),     # Yellow ball at left of cross because it had to go somewhere
-    (0, 0, 0, 0, 0, 0),             # Black point at 0,0,0
-    (-1, 0, -1, 0, 0, 0),             # Black point at -1,0,-1
-    (-1, 0, 1, 0, 0, 0),             # Black point at -1,0,1
-    (1, 0, -1, 0, 0, 0),             # Black point at 1,0,-1
-    (1, 0, 1, 0, 0, 0),             # Black point at 1,0,1
-    (0, 1, 0, 0, 0, 0),             # Black point at cross
-    (0, 1.1, 0, 0, 0, 0),           # Black point at forward spar
-]
-#
-# Version 3 calibration cross: the one with the leds and the uneven arms
-#
-POINTS_V3 = [
-    (-0.26, 1.04, 0, 255, 0, 0),        # Red LED at left of the cross
-    (0, 1.44, 0, 0, 0, 255),        # Blue LED at top of the cross (because the sky is blue)
-    (0, 1.31, -0.26, 0, 255, 0),   # Green LED pointing towards the viewer (-Z)
-    (0.26, 1.17, 0, 255, 255, 0),     # Yellow LED at right of cross because it had to go somewhere
-    (0, 0, 0, 0, 0, 0),             # Black point at 0,0,0
-    (-1, 0, -1, 0, 0, 0),             # Black point at -1,0,-1
-    (-1, 0, 1, 0, 0, 0),             # Black point at -1,0,1
-    (1, 0, -1, 0, 0, 0),             # Black point at 1,0,-1
-    (1, 0, 1, 0, 0, 0),             # Black point at 1,0,1
-]
-#
-# VideoLat calibration device, for frontal camera in desktop position
-POINTS_VIDEOLAT = [
-    (-0.040, 1.275,  0.000, 127, 127, 127),    # topleft back row
-    ( 0.040, 1.275,  0.000, 127, 127, 127),    # topright back row
-    (-0.015, 1.250, -0.025, 127, 127, 127),    # topleft front row
-    ( 0.015, 1.250, -0.025, 127, 127, 127),    # topright front row
-    (-0.015, 1.225, -0.025, 127, 127, 127),    # bottomleft front row
-    ( 0.015, 1.225, -0.025, 127, 127, 127),    # bottomright front row
-    (-0.040, 1.200,  0.000, 127, 127, 127),    # bottomleft back row
-    ( 0.040, 1.200,  0.000, 127, 127, 127),    # bottomright back row
-]
-#
-# Rubiks cube
-RH = 0.028 # Half the width/height/depth of the cube
-POINTS_RUBIK = [
-    (-RH, 1+RH, 0, 127, 127, 127),  # topleft
-    (+RH, 1+RH, 0, 127, 127, 127),  # topright
-    (-RH, 1-RH, 0, 127, 127, 127),  # botleft
-    (+RH, 1-RH, 0, 127, 127, 127),  # botright
-]
+from .targets import targets
       
 def main():
     parser = argparse.ArgumentParser(description="Calibrate cwipc_realsense2 capturer")
@@ -67,11 +17,10 @@ def main():
     parser.add_argument("--reuse", action="store_true", help=f"Reuse existing {DEFAULT_FILENAME}")
     parser.add_argument("--nograb", metavar="PLYFILE", action="store", help=f"Don't use grabber but use .ply file grabbed earlier, using {DEFAULT_FILENAME} from same directory.")
     parser.add_argument("--noinspect", action="store_true", help="Don't inspect pointclouds after grabbing")
+    parser.add_argument("--target", action="store", metavar="NAME", default="a4", help="Specify which target to use for coarse calibration. --list to see options.")
+    parser.add_argument("--list", action="store_true", help="List available targets")
     parser.add_argument("--nocoarse", action="store_true", help="Skip coarse (manual) calibration step")
     parser.add_argument("--nofine", action="store_true", help="Skip fine (automatic) calibration step")
-    parser.add_argument("--crossv3", action="store_true", help="For coarse calibration, use version 3 calibration cross (with the LEDs) in stead of the v2 rubber ball cross")
-    parser.add_argument("--videolat", action="store_true", help="For coarse calibration, use videolat flattened pyramid in stead of the v2 rubber ball cross")
-    parser.add_argument("--rubik", action="store_true", help="For coarse calibration, use Rubiks cube, points on forward-looking face")
     parser.add_argument("--bbox", action="store", type=float, nargs=6, metavar="N", help="Set bounding box (in meters, xmin xmax etc) before fine calibration")
     parser.add_argument("--corr", action="store", type=float, metavar="D", help="Set fine calibration max corresponding point distance", default=0.01)
     parser.add_argument("--finspect", action="store_true", help="Visually inspect result of each fine calibration step")
@@ -79,17 +28,15 @@ def main():
     parser.add_argument("--height", type=twofloats, action="store", metavar="MIN,MAX", help="Min and max Y value in meters, sets height filter for pointclouds")
     args = parser.parse_args()
     bbox = None
+    if args.list:
+        for name, target in targets.items():
+            print(f'{name}\n\t{target["description"]}')
+        sys.exit(0)
     if args.bbox:
         bbox = args.bbox
         assert len(bbox) == 6
         assert type(1.0*bbox[0]*bbox[1]*bbox[2]*bbox[3]*bbox[4]*bbox[5]) == float
-    refpoints = POINTS_V2
-    if args.crossv3:
-        refpoints = POINTS_V3
-    if args.videolat:
-        refpoints = POINTS_VIDEOLAT
-    if args.rubik:
-        refpoints = POINTS_RUBIK
+    refpoints = targets[args.target]["points"]
     prog = Calibrator(refpoints)
     if args.height:
         prog.setheight(*args.height)
---
+++ python/cwipc/scripts/cwipc_calibrate/targets.py
@@ -0,0 +1,77 @@
+from collections import OrderedDict
+
+# Rubiks cube
+RH = 0.028 # Half the width/height/depth of the cube
+
+targets=OrderedDict(
+    target_v2 = dict(
+        description="Cross with forward-point strut and balls",
+        points=[
+            (0.25, 1, 0, 255, 0, 0),        # Red ball at Right of the cross
+            (0, 1.25, 0, 0, 0, 255),        # Blue ball at top of the cross (because the sky is blue)
+            (0, 1.1, -0.25, 255, 127, 0),   # Orange ball pointing towards the viewer (-Z) because everyone likes orange
+            (-0.25, 1, 0, 255, 255, 0),     # Yellow ball at left of cross because it had to go somewhere
+            (0, 0, 0, 0, 0, 0),             # Black point at 0,0,0
+            (-1, 0, -1, 0, 0, 0),             # Black point at -1,0,-1
+            (-1, 0, 1, 0, 0, 0),             # Black point at -1,0,1
+            (1, 0, -1, 0, 0, 0),             # Black point at 1,0,-1
+            (1, 0, 1, 0, 0, 0),             # Black point at 1,0,1
+            (0, 1, 0, 0, 0, 0),             # Black point at cross
+            (0, 1.1, 0, 0, 0, 0),           # Black point at forward spar
+        ]
+    ),
+    target_v3 = dict(
+        description="Cross with uneven arms and LEDs",
+        points=[
+            (-0.26, 1.04, 0, 255, 0, 0),        # Red LED at left of the cross
+            (0, 1.44, 0, 0, 0, 255),        # Blue LED at top of the cross (because the sky is blue)
+            (0, 1.31, -0.26, 0, 255, 0),   # Green LED pointing towards the viewer (-Z)
+            (0.26, 1.17, 0, 255, 255, 0),     # Yellow LED at right of cross because it had to go somewhere
+            (0, 0, 0, 0, 0, 0),             # Black point at 0,0,0
+            (-1, 0, -1, 0, 0, 0),             # Black point at -1,0,-1
+            (-1, 0, 1, 0, 0, 0),             # Black point at -1,0,1
+            (1, 0, -1, 0, 0, 0),             # Black point at 1,0,-1
+            (1, 0, 1, 0, 0, 0),             # Black point at 1,0,1
+        ]
+    ),
+    a4 = dict(
+        description="A4 paper target held at 1.7m high (bottom of paper)",
+        points=[
+            (-0.105, 1.7+0.297, 0, 0, 0, 255),  # topleft, blue
+            (+0.105, 1.7+0.297, 0, 255, 0, 0),  # topright, red
+            (-0.105, 1.7, 0, 255, 0, 255),  # botleft, pink
+            (+0.105, 1.7, 0, 0, 255, 255),  # botright, yellow
+        ]
+    ),    
+    a41m = dict(
+        description="A4 paper target held at 1m high (bottom of paper)",
+        points=[
+            (-0.105, 1+0.297, 0, 0, 0, 255),  # topleft, blue
+            (+0.105, 1+0.297, 0, 255, 0, 0),  # topright, red
+            (-0.105, 1, 0, 255, 0, 255),  # botleft, pink
+            (+0.105, 1, 0, 0, 255, 255),  # botright, yellow
+        ]
+    ),
+    rubik = dict(
+        description="Rubiks cube held at 1m with one face looking forward",
+        points=[
+            (-RH, 1+RH, 0, 127, 127, 127),  # topleft
+            (+RH, 1+RH, 0, 127, 127, 127),  # topright
+            (-RH, 1-RH, 0, 127, 127, 127),  # botleft
+            (+RH, 1-RH, 0, 127, 127, 127),  # botright
+        ]
+    ),
+    videolat = dict(
+        description="Videolat calibration target held at 1.25m high",
+        points=[
+            (-0.040, 1.275,  0.000, 127, 127, 127),    # topleft back row
+            ( 0.040, 1.275,  0.000, 127, 127, 127),    # topright back row
+            (-0.015, 1.250, -0.025, 127, 127, 127),    # topleft front row
+            ( 0.015, 1.250, -0.025, 127, 127, 127),    # topright front row
+            (-0.015, 1.225, -0.025, 127, 127, 127),    # bottomleft front row
+            ( 0.015, 1.225, -0.025, 127, 127, 127),    # bottomright front row
+            (-0.040, 1.200,  0.000, 127, 127, 127),    # bottomleft back row
+            ( 0.040, 1.200,  0.000, 127, 127, 127),    # bottomright back row
+        ]
+    ),
+)
