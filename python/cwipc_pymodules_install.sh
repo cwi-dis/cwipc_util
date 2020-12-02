92dd00e53eb20447023208ef8983f932193b5f3e
Jack Jansen <Jack.Jansen@cwi.nl>
2020-12-02 16:42:22 +0100

    Make pyinstall scripts work for embedded python.

--- python/cwipc_pymodules_install.sh
+++ python/cwipc_pymodules_install.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 # Find Actual INSTALL_PREFIX
 bindir=`dirname $0`
-installdir=`dirname $bindir`
-sharedir=$installdir/share
+installdir=`cd $bindir/.. ; pwd`
+sharedir="$installdir/share"
+# Determine Python to use: python3 on linux/osx, python on windows, or embeddable python
+python3=python3
 if ! python3 --version >/dev/null 2>&1 ; then
-	alias python3=python
+	python3=python
 fi
-(cd $sharedir/cwipc_util/python ; python3 setup.py install)
-if [ -d $sharedir/cwipc_realsense2/python ]; then
-	(cd $sharedir/cwipc_realsense2/python ; python3 setup.py install)
+if [ -f "$installdir/python37embedded/python.exe" ]; then
+	python3="$installdir/python37embedded/python.exe"
 fi
-if [ -d $sharedir/cwipc_kinect/python ]; then
-	(cd $sharedir/cwipc_kinect/python ; python3 setup.py install)
+
+(cd "$sharedir/cwipc_util/python" ; "$python3" setup.py install)
+if [ -d "$sharedir/cwipc_realsense2/python" ]; then
+	(cd "$sharedir/cwipc_realsense2/python" ; "$python3" setup.py install)
 fi
-(cd $sharedir/cwipc_codec/python ; python3 setup.py install)
+if [ -d "$sharedir/cwipc_kinect/python" ]; then
+	(cd "$sharedir/cwipc_kinect/python" ; "$python3" setup.py install)
+fi
+(cd "$sharedir/cwipc_codec/python" ; "$python3" setup.py install)
--- python/cwipc_pymodules_install_win.bat
+++ python/cwipc_pymodules_install_win.bat
@@ -1,23 +1,23 @@
-@echo off
+@echo on
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
 set bindir=%~dp0
+set python=python
+if exist %bindir%..\python37embedded\python.exe (
+	set python="%bindir%..\python37embedded\python.exe"
+)
 pushd %bindir%..\share\cwipc_util\python
-python setup.py install
+%python% setup.py install
 if exist %bindir%..\share\cwipc_realsense2\python\setup.py (
 	pushd %bindir%..\share\cwipc_realsense2\python
-	python setup.py install
+	%python% setup.py install
 )
 if exist %bindir%..\share\cwipc_kinect\python\setup.py (
 	pushd %bindir%..\share\cwipc_kinect\python
-	python setup.py install
-)
-if exist %bindir%..\share\cwipc_bla\python\setup.py (
-	pushd %bindir%..\share\cwipc_bla\python
-	python setup.py install
+	%python% setup.py install
 )
 pushd %bindir%..\share\cwipc_codec\python
-python setup.py install
+%python% setup.py install
 
 exit
