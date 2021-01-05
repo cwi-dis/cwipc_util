dcfb30f71c128f8b444023ec30955f82819ac67a
Jack Jansen <Jack.Jansen@cwi.nl>
2021-01-05 12:47:10 +0100

    Added a4floor target

--- python/cwipc/scripts/cwipc_calibrate/targets.py
+++ python/cwipc/scripts/cwipc_calibrate/targets.py
@@ -52,6 +52,15 @@ targets=OrderedDict(
             (+0.105, 1, 0, 255, 255, 0),  # botright, yellow
         ]
     ),
+    a4floor = dict(
+        description="A4 paper target on the floor, forward-facing side up",
+        points=[
+            (+0.105, -0.01, -0.148, 0, 0, 255),  # topleft, blue
+            (-0.105, -0.01, -0.148, 255, 0, 0),  # topright, red
+            (+0.105, -0.01, +0.148, 255, 0, 255),  # botleft, pink
+            (-0.105, -0.01, +0.148, 255, 255, 0),  # botright, yellow
+        ]
+    ),
     rubik = dict(
         description="Rubiks cube held at 1m with one face looking forward",
         points=[
