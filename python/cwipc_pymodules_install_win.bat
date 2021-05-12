1691cb1f0c17c6ce25e0316993b780b0025fd721
Jack Jansen <Jack.Jansen@cwi.nl>
2021-05-12 11:37:43 +0200

    Added missing quotes in batchfile installer.

--- python/cwipc_pymodules_install_win.bat
+++ python/cwipc_pymodules_install_win.bat
@@ -4,20 +4,20 @@ cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) &&
 
 set bindir=%~dp0
 set python=python
-if exist %bindir%..\python37embedded\python.exe (
+if exist "%bindir%..\python37embedded\python.exe" (
 	set python="%bindir%..\python37embedded\python.exe"
 )
 %python% -m pip install importlib.metadata
 %python% -m pip install %bindir%..\share\cwipc_util\python
-if exist %bindir%..\share\cwipc_realsense2\python\setup.py (
-	%python% -m pip install %bindir%..\share\cwipc_realsense2\python
+if exist "%bindir%..\share\cwipc_realsense2\python\setup.py" (
+	%python% -m pip install "%bindir%..\share\cwipc_realsense2\python"
 )
-if exist %bindir%..\share\cwipc_kinect\python\setup.py (
-	%python% -m pip install %bindir%..\share\cwipc_kinect\python
+if exist "%bindir%..\share\cwipc_kinect\python\setup.py" (
+	%python% -m pip install "%bindir%..\share\cwipc_kinect\python"
 )
-%python% -m pip install %bindir%..\share\cwipc_codec\python
+%python% -m pip install "%bindir%..\share\cwipc_codec\python"
 pushd %bindir%..
-if exist %bindir%..\python37embedded\Scripts\ (
+if exist "%bindir%..\python37embedded\Scripts\" (
     xcopy /y "%bindir%..\python37embedded\Scripts\cwipc_*" "%bindir%..\bin"
 )
 popd
