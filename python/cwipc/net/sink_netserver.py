4a105e31d4cf540d4845ae88180805b57b7925c7
Jack Jansen <Jack.Jansen@cwi.nl>
2021-06-07 23:23:57 +0200

    Factored network sink (as a TCP server) out of cwipc_forward

---
+++ python/cwipc/net/sink_netserver.py
@@ -0,0 +1,111 @@
+import os
+import threading
+import socket
+import select
+import time
+import queue
+import cwipc
+
+try:
+    import cwipc.codec
+except ModuleNotFoundError:
+    cwipc.codec = None
+
+class _Sink_NetServer(threading.Thread):
+    
+    SELECT_TIMEOUT=0.1
+    QUEUE_FULL_TIMEOUT=0.001
+    
+    def __init__(self, port, verbose=False, nodrop=False):
+        threading.Thread.__init__(self)
+        self.producer = None
+        self.queue = queue.Queue(maxsize=2)
+        self.verbose = verbose
+        self.nodrop = nodrop
+        self.stopped = False
+        self.started = False
+        self.times_forward = []
+        self.sizes_forward = []
+        self.bandwidths_forward = []
+        self.socket = socket.socket()
+        self.socket.bind(('', port))
+        self.socket.listen()
+         
+    def start(self):
+        threading.Thread.start(self)
+        self.started = True
+        
+    def stop(self):
+        self.stopped = True
+        if self.started:
+            self.join()
+        
+    def set_producer(self, producer):
+        self.producer = producer
+        
+    def is_alive(self):
+        return not self.stopped  
+        
+    def run(self):
+        while self.producer and self.producer.is_alive():
+            readable, _, _ = select.select([self.socket], [], [], self.SELECT_TIMEOUT)
+            if self.socket in readable:
+                t1 = time.time()
+                connSocket, other = self.socket.accept()
+                if self.verbose:
+                    print(f"netserver: accepted connection from {other}")
+                data = self.queue.get()
+                connSocket.sendall(data)
+                connSocket.close()
+                t2 = time.time()
+                connSocket = None
+                self.times_forward.append(t2-t1)
+                datasize = len(data)
+                self.sizes_forward.append(datasize)
+                self.bandwidths_forward.append(datasize/(t2-t1))
+        self.stopped = True
+        
+    def feed(self, pc):
+        cpc = self._encode_pc(pc)
+        try:
+            if self.nodrop:
+                self.queue.put(cpc)
+            else:
+                self.queue.put(cpc, timeout=self.QUEUE_FULL_TIMEOUT)
+        except queue.Full:
+            pass
+        pc.free()
+    
+    def _encode_pc(self, pc):
+        encparams = cwipc.codec.cwipc_encoder_params(False, 1, 1.0, 9, 85, 16, 0, 0)
+        enc = cwipc.codec.cwipc_new_encoder(params=encparams)
+        enc.feed(pc)
+        gotData = enc.available(True)
+        assert gotData
+        data = enc.get_bytes()
+        pc.free()
+        enc.free()
+        return data
+
+    def statistics(self):
+        self.print1stat('connection_duration', self.times_forward)
+        self.print1stat('datasize', self.sizes_forward)
+        self.print1stat('bandwidth', self.bandwidths_forward)
+        
+    def print1stat(self, name, values, isInt=False):
+        count = len(values)
+        if count == 0:
+            print('netserver: {}: count=0'.format(name))
+            return
+        minValue = min(values)
+        maxValue = max(values)
+        avgValue = sum(values) / count
+        if isInt:
+            fmtstring = 'netserver: {}: count={}, average={:.3f}, min={:d}, max={:d}'
+        else:
+            fmtstring = 'netserver: {}: count={}, average={:.3f}, min={:.3f}, max={:.3f}'
+        print(fmtstring.format(name, count, avgValue, minValue, maxValue))
+
+def cwipc_sink_netserver(port, verbose=False):
+    """Create a cwipc_sink object that serves compressed pointclouds on a TCP network port"""
+    return _Sink_NetServer(port, verbose=verbose)
--- python/cwipc/scripts/cwipc_forward.py
+++ python/cwipc/scripts/cwipc_forward.py
@@ -2,104 +2,13 @@ import sys
 import os
 import time
 import threading
-import socket
-import select
 import time
 import argparse
 import traceback
-import queue
 import cwipc
-try:
-    import cwipc.codec
-except ModuleNotFoundError:
-    cwipc.codec = None
+import cwipc.net.sink_netserver
 from ._scriptsupport import *
 
-class Forwarder(threading.Thread):
-    
-    SELECT_TIMEOUT=0.1
-    QUEUE_FULL_TIMEOUT=0.001
-    
-    def __init__(self, port, verbose=False, nodrop=False):
-        threading.Thread.__init__(self)
-        self.producer = None
-        self.queue = queue.Queue(maxsize=2)
-        self.verbose = verbose
-        self.nodrop = nodrop
-        self.stopped = False
-        self.times_forward = []
-        self.sizes_forward = []
-        self.bandwidths_forward = []
-        self.socket = socket.socket()
-        self.socket.bind(('', port))
-        self.socket.listen()
-         
-    def set_producer(self, producer):
-        self.producer = producer
-        
-    def is_alive(self):
-        return not self.stopped  
-        
-    def run(self):
-        while self.producer and self.producer.is_alive():
-            readable, _, _ = select.select([self.socket], [], [], self.SELECT_TIMEOUT)
-            if self.socket in readable:
-                t1 = time.time()
-                connSocket, other = self.socket.accept()
-                if self.verbose:
-                    print(f"forwarder: accepted connection from {other}")
-                data = self.queue.get()
-                connSocket.sendall(data)
-                connSocket.close()
-                t2 = time.time()
-                connSocket = None
-                self.times_forward.append(t2-t1)
-                datasize = len(data)
-                self.sizes_forward.append(datasize)
-                self.bandwidths_forward.append(datasize/(t2-t1))
-        self.stopped = True
-        
-    def feed(self, pc):
-        cpc = self._encode_pc(pc)
-        try:
-            if self.nodrop:
-                self.queue.put(cpc)
-            else:
-                self.queue.put(cpc, timeout=self.QUEUE_FULL_TIMEOUT)
-        except queue.Full:
-            pass
-        pc.free()
-    
-    def _encode_pc(self, pc):
-        encparams = cwipc.codec.cwipc_encoder_params(False, 1, 1.0, 9, 85, 16, 0, 0)
-        enc = cwipc.codec.cwipc_new_encoder(params=encparams)
-        enc.feed(pc)
-        gotData = enc.available(True)
-        assert gotData
-        data = enc.get_bytes()
-        pc.free()
-        enc.free()
-        return data
-
-    def statistics(self):
-        self.print1stat('connection_duration', self.times_forward)
-        self.print1stat('datasize', self.sizes_forward)
-        self.print1stat('bandwidth', self.bandwidths_forward)
-        
-    def print1stat(self, name, values, isInt=False):
-        count = len(values)
-        if count == 0:
-            print('forwarder: {}: count=0'.format(name))
-            return
-        minValue = min(values)
-        maxValue = max(values)
-        avgValue = sum(values) / count
-        if isInt:
-            fmtstring = 'forwarder: {}: count={}, average={:.3f}, min={:d}, max={:d}'
-        else:
-            fmtstring = 'forwarder: {}: count={}, average={:.3f}, min={:.3f}, max={:.3f}'
-        print(fmtstring.format(name, count, avgValue, minValue, maxValue))
-    
 def main():
     SetupStackDumper()
     parser = ArgumentParser(description="Forward pointcloud streams", formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -112,7 +21,7 @@ def main():
     sourceFactory, source_name = cwipc_genericsource_factory(args)
     source = sourceFactory()
     if not args.noforward:
-        forwarder = Forwarder(args.port, verbose=args.verbose)
+        forwarder = cwipc.net.sink_netserver.cwipc_sink_netserver(args.port, verbose=args.verbose)
     else:
         forwarder = None
 
@@ -127,7 +36,7 @@ def main():
     try:
         sourceThread.start()
 
-        if forwarder:
+        if forwarder and hasattr(forwarder, 'start'):
             forwarder.start()
             
         sourceThread.join()
@@ -142,8 +51,9 @@ def main():
     #
     sourceServer.stop()
     sourceThread.join()
-    if forwarder:
-        forwarder.join()
+    if forwarder and hasattr(forwarder, 'stop'):
+        forwarder.stop()
+    if forwarder and hasattr(forwarder, 'statistics'):
         forwarder.statistics()
     sourceServer.statistics()
     
