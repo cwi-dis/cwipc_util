c051d207bd45eaed51cd0657567ac798d9ba19ec
Jack Jansen <Jack.Jansen@cwi.nl>
2022-01-04 21:01:22 +0100

    Add radius_filter=0 to cameraconfig.xml.

--- python/cwipc/scripts/cwipc_calibrate/cameraconfig.py
+++ python/cwipc/scripts/cwipc_calibrate/cameraconfig.py
@@ -6,7 +6,7 @@ DEFAULT_CONFIGFILE="""<?xml version="1.0" ?>
     <CameraConfig version="2">
         <system  />
         <skeleton />
-        <postprocessing height_min="0" height_max="0" greenscreenremoval="0">
+        <postprocessing height_min="0" height_max="0" radius_filter="0" greenscreenremoval="0">
             <depthfilterparameters  />
         </postprocessing>
         <camera serial="0" type="">
