4147758594a0917a77f1b1e6fd33643472e26f27
Jack Jansen <Jack.Jansen@cwi.nl>
2022-02-12 00:38:34 +0100

    Modified so it reports pointcounts and amount of time used for the downsample, and added a define that let's use do the same downsample multiple times, for performance measurements.

--- apps/cwipc_downsample/cwipc_downsample.cpp
+++ apps/cwipc_downsample/cwipc_downsample.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 
+// Define PERFORMANCE_TEST_COUNT greater than 1 to do the downsample multiple times to test performance
+#define PERFORMANCE_TEST_COUNT 10
+
 #include "cwipc_util/api.h"
 
 int main(int argc, char** argv)
@@ -21,12 +24,21 @@ int main(int argc, char** argv)
         std::cerr << argv[0] << ": Error reading pointcloud from " << argv[2] << ": " << errorMessage << std::endl;
         return 1;
     }
-    std::cerr << "Read pointcloud successfully, " << pc->get_uncompressed_size() << " bytes (uncompressed)" << std::endl;
+    std::cerr << "Read pointcloud successfully, " << pc->get_uncompressed_size() << " bytes (uncompressed), " << pc->count() << " points" << std::endl;
     //
     // Voxelize
     //
-    cwipc *new_pc = cwipc_downsample(pc, voxelsize);
-    //
+    auto t0_wall = std::chrono::high_resolution_clock::now();
+    cwipc *new_pc = nullptr;
+    for(int i=0; i < PERFORMANCE_TEST_COUNT; i++) {
+        if (new_pc != nullptr) new_pc->free();
+        new_pc = cwipc_downsample(pc, voxelsize);
+    }
+    auto t1_wall = std::chrono::high_resolution_clock::now();
+    
+    double delta_wall = std::chrono::duration<double, std::milli>(t1_wall - t0_wall).count();
+	delta_wall /= PERFORMANCE_TEST_COUNT;
+    std::cerr << argv[0] << ": did " << PERFORMANCE_TEST_COUNT << " downsamples, " << delta_wall << " ms per call, " << new_pc->count() << " points" << std::endl;
     // Save pointcloud file
     //
     if (cwipc_write(argv[3], new_pc, NULL) < 0) {
