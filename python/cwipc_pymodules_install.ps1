d7d856f1302d244077f8c89150ac4377f4270ebf
Jack Jansen <Jack.Jansen@cwi.nl>
2022-06-30 15:42:15 +0200

    PSScriptRoot is already the bin directory (not the script filename)

--- python/cwipc_pymodules_install.ps1
+++ python/cwipc_pymodules_install.ps1
@@ -1,4 +1,4 @@
-$bindir = Split-Path $PSScriptRoot -Parent
+$bindir = $PSScriptRoot
 
 python -m pip --quiet install importlib.metadata
 python -m pip --quiet install --upgrade --find-links="$bindir\..\share\cwipc\python" cwipc_util cwipc_codec cwipc_realsense2 cwipc_codec
