4017ead50999df48111f206d0b7cf4451854f9f9
Jack Jansen <Jack.Jansen@cwi.nl>
2023-05-04 00:07:33 +0200

    Cater for existency of cwipc_python on macos when using a brew install

--- python/cwipc_pymodules_install.sh
+++ python/cwipc_pymodules_install.sh
@@ -4,23 +4,19 @@ bindir=`dirname $0`
 bindir=`cd $bindir ; pwd`
 installdir=`cd $bindir/.. ; pwd`
 sharedir="$installdir/share"
-# Determine Python to use: python3 on linux/osx, python on windows, or embeddable python
-python3=python3
-if ! python3 --version >/dev/null 2>&1 ; then
-	python3=python
+# Determine Python to use: cwipc_python on brew, python3 on linux/osx, python on windows
+myPython=cwipc_python
+if ! $myPython --version >/dev/null 2>&1 ; then
+	myPython=python3
 fi
-if [ -f "$installdir/python37embedded/python.exe" ]; then
-	python3="$installdir/python37embedded/python.exe"
-	# Workaround for embedded Python: add . to pythonpath
-	echo 'import sys ; sys.path.insert(0, "")' >> "$installdir/python37embedded/Lib/site-packages/sitecustomize.py"
+if ! $myPython --version >/dev/null 2>&1 ; then
+	myPython=python
+fi
+if ! $myPython --version >/dev/null 2>&1 ; then
+	echo "$0: Cannot find suitable Python" >&2
+	exit 1
 fi
 set -x
-"$python3" -m pip --quiet install importlib.metadata
-"$python3" -m pip --quiet uninstall --yes cwipc_util cwipc_codec cwipc_realsense2 cwipc_codec
-"$python3" -m pip --quiet install --upgrade --find-links="$sharedir/cwipc/python" cwipc_util cwipc_codec cwipc_realsense2 cwipc_codec
-#
-# Finally, link python-based programs into bin
-#
-if [ -d "$installdir/bin" -a -d "$installdir/python37embedded/Scripts" ]; then
-    ln -f "$installdir/python37embedded/Scripts/cwipc_"* "$installdir/bin/"
-fi
+"$myPython" -m pip --quiet install importlib.metadata
+"$myPython" -m pip --quiet uninstall --yes cwipc_util cwipc_codec cwipc_realsense2 cwipc_codec
+"$myPython" -m pip --quiet install --upgrade --find-links="$sharedir/cwipc/python" cwipc_util cwipc_codec cwipc_realsense2 cwipc_codec
