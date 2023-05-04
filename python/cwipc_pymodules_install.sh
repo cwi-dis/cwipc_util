78158d104b5a885225ba7cb1894df1bdd62764a3
Jack Jansen <Jack.Jansen@cwi.nl>
2023-05-04 11:57:51 +0200

    Allow overriding python to use with environment variable.

--- python/cwipc_pymodules_install.sh
+++ python/cwipc_pymodules_install.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # Find Actual INSTALL_PREFIX
 bindir=`dirname $0`
 bindir=`cd $bindir ; pwd`
 installdir=`cd $bindir/.. ; pwd`
 sharedir="$installdir/share"
 # Determine Python to use: cwipc_python on brew, python3 on linux/osx, python on windows
-myPython=cwipc_python
+myPython=${CWIPC_PYTHON:=cwipc_python}
 if ! $myPython --version >/dev/null 2>&1 ; then
 	myPython=python3
 fi
