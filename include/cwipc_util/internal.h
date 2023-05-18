4209102918f2fcc7c9e968ea4bbfa52ee0f9316d
Jack Jansen <Jack.Jansen@cwi.nl>
2023-05-19 00:19:22 +0200

    Header cwipc_util/internal.h contains base classes for capture configuration structures.

---
+++ include/cwipc_util/internal.h
@@ -0,0 +1,14 @@
+#ifndef _cwipc_util_internal_h_
+#define _cwipc_util_internal_h_
+
+#include "cwipc_util/api.h"
+
+struct CwipcBaseCameraConfig {
+	std::string type;
+};
+
+struct CwipcBaseCaptureConfig {
+	std::string type;
+};
+
+#endif // _cwipc_util_internal_h_
