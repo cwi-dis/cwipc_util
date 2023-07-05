d75765f389a114649321fb142e78fed82c7e9843
BEELZEBUB\DIS <cwidisgroup@gmail.com>
2023-07-05 17:24:40 +0200

    Part of the implementation

--- include/cwipc_util/internal.h
+++ include/cwipc_util/internal.h
@@ -31,5 +31,7 @@ class cwipc_tiledsource;
 typedef int _cwipc_functype_count_devices();
 typedef cwipc_tiledsource* _cwipc_func_capturer_factory(const char *configFilename, char **errorMessage, uint64_t apiVersion);
 
-_CWIPC_UTIL_EXPORT int _cwipc_register_capturer(const char *name, bool isCamera, _cwipc_functype_count_devices *countFunc, _cwipc_func_capturer_factory *factoryFunc);
+extern "C" {
+	_CWIPC_UTIL_EXPORT int _cwipc_register_capturer(const char* name, _cwipc_functype_count_devices* countFunc, _cwipc_func_capturer_factory* factoryFunc);
+}
 #endif // _cwipc_util_internal_h_
--- src/cwipc_capturer.cpp
+++ src/cwipc_capturer.cpp
@@ -6,6 +6,9 @@
 #include <pcl/common/transforms.h>
 #include <pcl/common/common_headers.h>
 
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
 #ifdef WIN32
 #define _CWIPC_UTIL_EXPORT __declspec(dllexport)
 #else
@@ -15,6 +18,13 @@
 #include "cwipc_util/api.h"
 #include "cwipc_util/internal.h"
 
+struct capturer {
+    std::string name;
+    _cwipc_functype_count_devices* countFunc;
+    _cwipc_func_capturer_factory* factoryFunc;
+};
+
+std::vector<struct capturer> all_capturers;
 
 cwipc_tiledsource *
 cwipc_capturer(const char *configFilename, char **errorMessage, uint64_t apiVersion)
@@ -27,13 +37,93 @@ cwipc_capturer(const char *configFilename, char **errorMessage, uint64_t apiVers
         }
 		return NULL;
 	}
+    if (strcmp(configFilename, "auto") == 0) {
+        // Find any capturer that corresponds to a physical device (i.e. has a count function) that is available
+        struct capturer* candidate = nullptr;
+        for (auto& c : all_capturers) {
+            if (c.countFunc != nullptr) {
+                if (c.countFunc()) {
+                    // This is a candidate. Check that there are no others.
+                    if (candidate == nullptr) {
+                        candidate = &c;
+                    }
+                    else {
+                        if (errorMessage) {
+                            char* msgbuf = (char*)malloc(1024);
+                            snprintf(msgbuf, 1024, "cwipc_capturer: auto: cameras of multiple types found");
+                            *errorMessage = msgbuf;
+                        }
+                        return nullptr;
+                    }
+                }
+            }
+        }
+        if (candidate == nullptr) {
+            if (errorMessage) {
+                char* msgbuf = (char*)malloc(1024);
+                snprintf(msgbuf, 1024, "cwipc_capturer: auto: no supported cameras found");
+                *errorMessage = msgbuf;
+            }
+            return nullptr;
+        }
+        return candidate->factoryFunc(configFilename, errorMessage, apiVersion);
+    }
+    // Assure we are passed a json cameraconfig
+    if (configFilename == nullptr) {
+        configFilename = "cameraconfig.json";
+    }
+    std::string cameraType;
+    json json_data;
+    if (configFilename[0] == '{') {
+        // Special case: a string starting with { is considered a JSON literal
+        
+        try {
+            json_data = json::parse(configFilename);
+        }
+        catch (const std::exception& e) {
+            if (errorMessage) {
+                char* msgbuf = (char*)malloc(1024);
+                snprintf(msgbuf, 1024, "cwipc_capturer: auto: JSON parse error");
+                *errorMessage = msgbuf;
+            }
+            return nullptr;
+        }
+    }
+    // Otherwise we check the extension. It can be .xml or .json.
+    const char* extension = strrchr(configFilename, '.');
+    if (strcmp(extension, ".json") == 0) {
+        try {
+            std::ifstream f(configFilename);
+            if (!f.is_open()) {
+                if (errorMessage) {
+                    char* msgbuf = (char*)malloc(1024);
+                    snprintf(msgbuf, 1024, "cwipc_capturer: auto: cannot open \"%s\"", configFilename);
+                    *errorMessage = msgbuf;
+                }
+                return nullptr;
+            }
+            json_data = json::parse(f);
+
+        }
+        catch (const std::exception& e) {
+            if (errorMessage) {
+                char* msgbuf = (char*)malloc(1024);
+                snprintf(msgbuf, 1024, "cwipc_capturer: auto: JSON parse error");
+                *errorMessage = msgbuf;
+            }
+            return nullptr;
+        }
+    }
+    std::string type;
+    json_data.at("type").get_to(type);
+
     char* msgbuf = (char*)malloc(1024);
-    snprintf(msgbuf, 1024, "cwipc_capturer: not yet implemented");
+    snprintf(msgbuf, 1024, "cwipc_capturer: auto: cannot determine camera type from \"%s\"", configFilename);
     *errorMessage = msgbuf;
-	return NULL;
+    return nullptr;
 }
 
-int _cwipc_register_capturer(const char *name, bool isCamera, _cwipc_functype_count_devices *countFunc, _cwipc_func_capturer_factory *factoryFunc) {
+int _cwipc_register_capturer(const char *name, _cwipc_functype_count_devices *countFunc, _cwipc_func_capturer_factory *factoryFunc) {
     fprintf(stderr, "xxxjack _cwipc_register_capturer(%s, ...)\n", name);
     return 0;
 }
