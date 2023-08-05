228c08e2ca453d9f4318e6e29bd14d375a4bd825
Jack Jansen <Jack.Jansen@cwi.nl>
2023-08-05 17:28:24 +0200

    Got rid of very old debug code.

--- apps/cwipc_dump2ply/cwipc_dump2ply.cpp
+++ apps/cwipc_dump2ply/cwipc_dump2ply.cpp
@@ -24,22 +24,10 @@ int main(int argc, char** argv)
     //
     // Save
     //
-    if (strcmp(argv[2], "-") == 0) {
-        // copy-uncompressed in stead of save (for performance testing)
-        size_t nbytes = obj->get_uncompressed_size();
-        cwipc_point *points = (cwipc_point *)malloc(nbytes);
-        if (points == NULL) {
-            std::cerr << argv[0] << ": out of memory" << std::endl;
-            return 1;
-        }
-        obj->copy_uncompressed(points, nbytes);
-        std::cerr << argv[0] << ": Skipping save" << std::endl;
-    } else {
-        int status = cwipc_write(argv[2], obj, &message);
-        if (status < 0) {
-            std::cerr << argv[0] << ": Cannot save pointcloud to ply: " << message << std::endl;
-            return 1;
-        }
+    int status = cwipc_write(argv[2], obj, &message);
+    if (status < 0) {
+        std::cerr << argv[0] << ": Cannot save pointcloud to ply: " << message << std::endl;
+        return 1;
     }
     return 0;
 }
