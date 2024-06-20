c140bcbaa350d0b02a0e8be78cb880dc4b226e0f
Jack Jansen <Jack.Jansen@cwi.nl>
2024-06-21 00:02:06 +0200

    Pin numpy version at 1.x until we can find out why numpy 2.0 and open3d 0.18 cause a segfault. Fixes #109

--- python/setup.py
+++ python/setup.py
@@ -121,7 +121,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     #
-    install_requires=['numpy', 'open3d', 'Pillow', 'opencv-python', 'scipy', 'matplotlib'],
+    install_requires=['numpy<2.0.0', 'open3d', 'Pillow', 'opencv-python', 'scipy', 'matplotlib'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
