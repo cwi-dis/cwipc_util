e08379ace101e993bec95cb66876b2913d545011
Jack Jansen <Jack.Jansen@cwi.nl>
2025-01-19 01:50:22 +0100

    Changed version number.

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 
 def get_version():
-    CWIPC_VERSION="7.5+unknown"
+    CWIPC_VERSION="7.6+unknown"
     if 'CWIPC_VERSION' in os.environ:
         CWIPC_VERSION=os.environ['CWIPC_VERSION']
     return CWIPC_VERSION
