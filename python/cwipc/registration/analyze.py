Filtering the points to include in the correspondence computation was wrong: it used a distance interval of [mean-std, mean+std] but it should be using [0, mean+std].

python/cwipc/registration/analyze.py
@@ -215,7 +215,8 @@ class BaseRegistrationAnalyzer(AnalysisAlgorithm, BaseAlgorithm):
                 if self.verbose:
                     print(f"\t\tstep {filterstep}: mean={mean}, std={stddev}, nPoint={len(overlap_distances)}")
                 # Create an array of booleans for all distances we want to keep, and filter on that.
-                filter = np.logical_and(overlap_distances <= (mean+stddev), overlap_distances >= (mean-stddev))
+                # filter = np.logical_and(overlap_distances <= (mean+stddev), overlap_distances >= (mean-stddev))
+                filter = np.logical_and(overlap_distances <= (mean+stddev), overlap_distances >= 0)
                 overlap_distances = overlap_distances[filter]
             # Last step: see how many points are below our new-found correspondence
             corr = mean + stddev