bacf85503f99aa8c1a84be8b8eada6467256faf4
Jack Jansen <Jack.Jansen@cwi.nl>
2025-02-26 22:40:16 +0100

    Allow setting the reference point cloud. If it is not set then the point clouds from all cameras except the camera-under-test will be combined to form the reference point cloud.

--- python/cwipc/registration/fine.py
+++ python/cwipc/registration/fine.py
@@ -6,7 +6,7 @@ import scipy.spatial
 import open3d
 from .. import cwipc_wrapper, cwipc_tilefilter, cwipc_from_points, cwipc_join
 from .abstract import *
-from .util import transformation_identity, BaseAlgorithm, cwipc_transform
+from .util import transformation_identity, BaseAlgorithm, cwipc_transform, cwipc_point_numpy_matrix_value_type
 
 RegistrationResult = open3d.pipelines.registration.RegistrationResult
 
@@ -14,13 +14,25 @@ class RegistrationComputer(AlignmentAlgorithm, BaseAlgorithm):
     """Compute the registration for a pointcloud.
     This is the base class, which actually does nothing and always returns a fixed unit matrix.
     """
+    correspondence : float
+    our_pointcloud : Optional[cwipc_wrapper]
+    reference_pointcloud : Optional[cwipc_wrapper]
+    our_points_nparray : Optional[cwipc_point_numpy_matrix_value_type]
+    reference_points_nparray : Optional[cwipc_point_numpy_matrix_value_type]
+    registration_result : RegistrationResult
 
     def __init__(self):
         BaseAlgorithm.__init__(self)
         self.correspondence = 1 # Distance in meters between candidate points to be matched, so this is a ridiculously large value
+        self.our_pointcloud = None
+        self.our_points_nparray = None
+        self.reference_points_nparray = None
 
     def set_correspondence(self, correspondence) -> None:
         self.correspondence = correspondence
+
+    def set_reference_pointcloud(self, pc : cwipc_wrapper) -> None:
+        self.reference_pointcloud = pc
 
     def run(self, target: Optional[int]=None) -> bool:
         """Run the algorithm"""
@@ -37,28 +49,37 @@ class RegistrationComputer(AlignmentAlgorithm, BaseAlgorithm):
             assert False, f"target camera {target} not found"
         self.our_pointcloud = self.per_camera_pointclouds[targetIndex]
         self.our_points_nparray = self.our_pointcloud.get_numpy_matrix(onlyGeometry=True)
-
-        other_nparrays = [
-            cam_pc.get_numpy_matrix(onlyGeometry=True) for cam_pc in self.per_camera_pointclouds if cam_pc != self.our_pointcloud
-        ]
-        self.other_points_nparray = np.concatenate(other_nparrays)
+        # If set_reference_pointcloud() was not called, we'll use the combined point clouds of all other cameras as reference
+        if self.reference_pointcloud is None:
+            other_nparrays = [
+                cam_pc.get_numpy_matrix(onlyGeometry=True) for cam_pc in self.per_camera_pointclouds if cam_pc != self.our_pointcloud
+            ]
+            self.reference_points_nparray = np.concatenate(other_nparrays)
+        else:
+            self.reference_points_nparray = self.reference_pointcloud.get_numpy_matrix(onlyGeometry=True)
 
     def get_result_transformation(self, nonverbose=False) -> RegistrationTransformation:
         return transformation_identity()
     
     def get_result_pointcloud(self) -> cwipc_wrapper:
         pc = self.our_pointcloud
+        assert pc is not None
         transform = self.get_result_transformation(nonverbose=True)
         new_pc = cwipc_transform(pc, transform)
         return new_pc
     
     def get_result_pointcloud_full(self) -> cwipc_wrapper:
         part_pc = self.get_result_pointcloud()
-        for other_pc in self.per_camera_pointclouds:
-            if other_pc != self.our_pointcloud:
-                new_part_pc = cwipc_join(part_pc, other_pc)
-                part_pc.free()
-                part_pc = new_part_pc
+        if self.reference_pointcloud is not None:
+            new_part_pc = cwipc_join(part_pc, self.reference_pointcloud)
+            part_pc.free()
+            part_pc = new_part_pc
+        else:
+            for other_pc in self.per_camera_pointclouds:
+                if other_pc != self.our_pointcloud:
+                    new_part_pc = cwipc_join(part_pc, other_pc)
+                    part_pc.free()
+                    part_pc = new_part_pc
         return part_pc
 
 class RegistrationComputer_ICP_Point2Point(RegistrationComputer):
@@ -96,7 +117,7 @@ class RegistrationComputer_ICP_Point2Point(RegistrationComputer):
     
     def _get_target_pointcloud(self) -> open3d.geometry.PointCloud:
         target_pointcloud = open3d.geometry.PointCloud()
-        target_pointcloud.points = open3d.utility.Vector3dVector(self.other_points_nparray)
+        target_pointcloud.points = open3d.utility.Vector3dVector(self.reference_points_nparray)
         return target_pointcloud
     
     def _get_max_correspondence_distance(self) -> float:
@@ -152,7 +173,7 @@ class RegistrationComputer_ICP_Point2Plane(RegistrationComputer):
     
     def _get_target_pointcloud(self) -> open3d.geometry.PointCloud:
         target_pointcloud = open3d.geometry.PointCloud()
-        target_pointcloud.points = open3d.utility.Vector3dVector(self.other_points_nparray)
+        target_pointcloud.points = open3d.utility.Vector3dVector(self.reference_points_nparray)
         target_pointcloud.estimate_normals(self._get_normal_search_strategy())
         return target_pointcloud
     
