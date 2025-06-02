732866fea9d93ede97cb7ab0b64b9cf8b02aead8
Jack Jansen <Jack.Jansen@cwi.nl>
2025-06-02 22:38:43 +0200

    Added noise(distance) filter. Adds a random vector of random length (up to distance) to each point.

--- python/cwipc/filters/__init__.py
+++ python/cwipc/filters/__init__.py
@@ -4,9 +4,9 @@ import importlib
 import importlib.util
 from .abstract import cwipc_abstract_filter
 from ..net.abstract import *
-from . import passthrough, analyze, voxelize, transform, transform44, crop, remove_outliers, colorize
+from . import passthrough, analyze, voxelize, transform, transform44, crop, remove_outliers, colorize, noise
 
-all_filters = [passthrough, analyze, voxelize, transform, transform44, crop, remove_outliers, colorize]
+all_filters = [passthrough, analyze, voxelize, transform, transform44, crop, remove_outliers, colorize, noise]
 
 def help() -> None:
     print("A builtin filter can be specified by name (for example passthrough) or as name with arguments (for example passthrough()).", file=sys.stderr)
