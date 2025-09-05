c05910eda50882e0f35411a568fe8e0c2524766c
Jack Jansen <Jack.Jansen@cwi.nl>
2025-09-05 14:10:46 +0200

    Implemented help, version, check and python commands. Closes #36.

--- python/cwipc/__main__.py
+++ python/cwipc/__main__.py
@@ -1,7 +1,8 @@
 import sys
+import os
 from typing import List, Tuple
 import pkgutil
-from . import scripts
+from . import scripts, cwipc_get_version
 
 MAIN_COMMANDS = ['view', 'play', 'grab', 'register']
 
@@ -39,14 +40,54 @@ def help():
             print(f"  {s:20} - {doc}", file=sys.stderr)
         else:
             print(f"  {s:20}", file=sys.stderr)
+    print("\nSpecial commands:", file=sys.stderr)
+    print("  help                 - show this help message", file=sys.stderr)
+    print("  version              - show cwipc version", file=sys.stderr)
+    print("  check                - check if cwipc is correctly installed, or fix installation", file=sys.stderr)
+    print("  python               - run python that has cwipc package installed", file=sys.stderr)
     print("\nUse 'cwipc <command> -h' for help on a specific command.", file=sys.stderr)
     print("See http://github.com/cwi-dis/cwipc for more information.", file=sys.stderr)
+
+def run_version() -> int:
+    print(cwipc_get_version())
+    return 0
+
+def run_check() -> int:
+    mydir = os.path.dirname(__file__)
+    while True:
+        totry = [
+            os.path.join(mydir, 'bin', 'cwipc_check.exe'),
+            os.path.join(mydir, 'bin', 'cwipc_check'),
+            os.path.join(mydir, 'build', 'bin', 'cwipc_check'),
+            os.path.join(mydir, 'build', 'bin', 'RelWithDebInfo', 'cwipc_check.exe'),
+            os.path.join(mydir, 'build', 'bin', 'Release', 'cwipc_check.exe'),
+        ]
+        for path in totry:
+            if os.path.exists(path):
+                check_script = path
+                os.execv(check_script, [check_script] + sys.argv[2:])
+                assert 0, "execv failed"
+        parent = os.path.dirname(mydir)
+        if parent == mydir:
+            print("Could not find cwipc_check executable", file=sys.stderr)
+            return 1
+        mydir = parent
+    
+def run_python() -> None:
+    os.execv(sys.executable, [sys.executable] + sys.argv[2:])
+    assert 0, "execv failed"
 
 def main():
     if len(sys.argv) < 2 or sys.argv[1] in ('-h', '--help', 'help'):
         help()
         sys.exit(1)
     command = sys.argv[1]
+    if command in ('-v', '--version', 'version'):
+        sys.exit(run_version())
+    if command == "check":
+        sys.exit(run_check())
+    if command == "python":
+        sys.exit(run_python())
     try:
         mod = __import__(f'cwipc.scripts.cwipc_{command}', fromlist=[''])
     except ImportError:
