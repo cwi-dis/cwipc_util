760083ca0e165d621a2dd1e803d2d0be63dd490f
Jack Jansen <Jack.Jansen@cwi.nl>
2025-09-10 12:05:37 +0200

    Updated default version string.

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 
 def get_version():
-    CWIPC_VERSION="7.6+unknown"
+    CWIPC_VERSION="7.7+unknown"
     if 'CWIPC_VERSION' in os.environ:
         CWIPC_VERSION=os.environ['CWIPC_VERSION']
     return CWIPC_VERSION
