ba9faff621e2b4ec134bd38892d7c41e140ab039
BEELZEBUB\jack <Jack.Jansen@cwi.nl>
2025-12-17 23:37:09 +0100

    Moved part of the getting and putting of config values to/from json to the base classes.

--- include/cwipc_util/internal.h
+++ include/cwipc_util/internal.h
@@ -4,6 +4,9 @@
 #include "cwipc_util/api_pcl.h"
 #include "cwipc_util/api.h"
 #include <thread>
+
+#include <nlohmann/json.hpp>
+
 //
 // This file contains definitions of types and functions that are not user-visible,
 // but that are provided by cwipc_util for use by other modules.
@@ -25,12 +28,37 @@ extern "C" {
     enum cwipc_log_level { LOG_ERROR, LOG_WARNING, LOG_TRACE, LOG_DEBUG };
     _CWIPC_UTIL_EXPORT void cwipc_log(cwipc_log_level level, std::string module, std::string message);
 };
+
+
+using json = nlohmann::json;
+
+#define _CWIPC_CONFIG_JSON_GET(jsonobj, name, config, attr) if (jsonobj.contains(#name)) jsonobj.at(#name).get_to(config.attr)
+#define _CWIPC_CONFIG_JSON_PUT(jsonobj, name, config, attr) jsonobj[#name] = config.attr
+
 struct CwipcBaseCameraConfig {
     std::string type;
+
+    virtual void _from_json(const json& json_data) {
+        json_data.at("type").get_to(type);
+    }
+    virtual void _to_json(json& json_data) {
+        json_data["type"] = type;
+    }
 };
 
 struct CwipcBaseCaptureConfig {
     std::string type;
+
+    virtual std::string to_string() = 0;
+    virtual bool from_string(const char* buffer, std::string typeWanted) = 0;
+    virtual bool from_file(const char* filename, std::string typeWanted) = 0;
+    virtual void _from_json(const json& json_data) {
+        json_data.at("type").get_to(type);
+    }
+    virtual void _to_json(json& json_data) {
+        json_data["type"] = type;
+        json_data["version"] = 4;
+    }
 };
 
 class CwipcBaseCamera {
