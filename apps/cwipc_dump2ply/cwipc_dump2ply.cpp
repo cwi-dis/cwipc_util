27561ec175a16690a4de2a4a45a807548c99d834
Jack Jansen <Jack.Jansen@cwi.nl>
2026-02-05 22:23:43 +0100

    Added missing free().

--- apps/cwipc_dump2ply/cwipc_dump2ply.cpp
+++ apps/cwipc_dump2ply/cwipc_dump2ply.cpp
@@ -16,8 +16,8 @@ int main(int argc, char** argv) {
     //
     // Read pointcloud file
     //
-    cwipc_pointcloud *obj = cwipc_read_debugdump(argv[1], &message, CWIPC_API_VERSION);
-    if (obj == NULL) {
+    cwipc_pointcloud *pc = cwipc_read_debugdump(argv[1], &message, CWIPC_API_VERSION);
+    if (pc == NULL) {
         std::cerr << argv[0] << ": Cannot read pointcloud from dump: " << message << std::endl;
         return 1;
     }
@@ -25,11 +25,12 @@ int main(int argc, char** argv) {
     //
     // Save
     //
-    int status = cwipc_write(argv[2], obj, &message);
+    int status = cwipc_write(argv[2], pc, &message);
     if (status < 0) {
         std::cerr << argv[0] << ": Cannot save pointcloud to ply: " << message << std::endl;
         return 1;
     }
+    pc->free();
     if (cwipc_dangling_allocations(true)) return 1;
     return 0;
 }
